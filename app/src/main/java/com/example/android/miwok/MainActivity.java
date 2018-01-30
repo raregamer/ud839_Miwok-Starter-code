bf3db031f73b321c952b1a9ade1523fc4e3de222
Samuel <raregamer@hotmail.com>
2018-01-30 10:02:16 -0800

    Added a list view to NumbersActivity to display words. Also changed TextView colors to a non final state.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView numbers = (TextView) findViewById(R.id.numbers);
         TextView family = (TextView) findViewById(R.id.family);
-        final TextView colors = (TextView) findViewById(R.id.colors);
+        TextView colors = (TextView) findViewById(R.id.colors);
         TextView phrases = (TextView) findViewById(R.id.phrases);
 
         numbers.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,6 +19,16 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
         ArrayList<String> words = new ArrayList<String>();
         Collections.addAll(words,"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten");
+
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,words);
+
+        ListView listView = findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
+
+
 
         for (int i = 0; i < words.size();i++) {
 
